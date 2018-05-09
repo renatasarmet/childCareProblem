75e6d87dbab48942c20ebe4d562b586995043a2d
renatasarmet <renatassmendes@hotmail.com>
2018-05-09 12:04:47 -0300

    Sai adulto feito!

--- chieldCareProblem.c
+++ chieldCareProblem.c
@@ -7,19 +7,36 @@
 #include <semaphore.h>
 
 #define N_ITENS 30
-char buffer[N_ITENS+N_ITENS*3 ];
-int contBuffer =0;
+//char buffer[N_ITENS+N_ITENS*3 ];
+//int contBuffer =0;
+int buffer_adulto[N_ITENS];
+int buffer_crianca[3 * N_ITENS];
+
+int qtd_adulto = 0;
+int qtd_crianca = 0;
+
 sem_t sem_adulto;
 sem_t sem_crianca;
 
-void* adulto(void *v) {		
+sem_t pos_vazia_adulto;
+sem_t pos_ocupada_adulto;
+
+sem_t pos_vazia_crianca;
+sem_t pos_ocupada_crianca;
+
+int inicio_adulto = 0, fim_adulto = 0, inicio_crianca = 0, fim_crianca = 0;
+
+void* entra_adulto(void *v) {		
 	int i;
 	for(i=0;i<N_ITENS;i++){
-
+		sem_wait(&pos_vazia_adulto);
 		sem_wait(&sem_adulto);
-    	printf("Adulto, item = %d.\n", i);     
-		buffer[contBuffer] = "A";
-        contBuffer++;
+		qtd_adulto += 1;
+    	printf("Entrando adulto, item = %d. Tenho qtd = %d\n", i, qtd_adulto);     
+    	fim_adulto = (fim_adulto + 1) % N_ITENS;
+		buffer_adulto[fim_adulto] = i;
+		sem_post(&pos_ocupada_adulto);
+
 		sem_post(&sem_crianca);
 		sem_post(&sem_crianca);
 		sem_post(&sem_crianca);
@@ -31,17 +48,19 @@ void* adulto(void *v) {
 }
 
 
-void* crianca(void *v) {
-
-	int qtd_crianca;
+void* entra_crianca(void *v) {
+int quantas_criancas;
 	int i;
 	for(i=0;i< 3 * N_ITENS;i++){
-		sem_wait(&sem_crianca);
-    	printf("Crianca, item = %d.\n", i);  
-		buffer[contBuffer] = "C";
-		contBuffer++;
-		sem_getvalue(&sem_crianca, &qtd_crianca);
-		if (qtd_crianca == 0){
+		sem_wait(&pos_vazia_crianca);
+		sem_wait(&sem_crianca);  
+		qtd_crianca += 1;
+    	printf("Entrando crianca, item = %d. Tenho qtd = %d\n", i, qtd_crianca);
+		fim_crianca = (fim_crianca + 1) % N_ITENS;
+		buffer_crianca[fim_crianca] = i;
+
+		sem_getvalue(&sem_crianca, &quantas_criancas);
+		if (quantas_criancas == 0){
 			sem_post(&sem_adulto);
 		}
 		sleep(random() % 3);
@@ -51,21 +70,58 @@ void* crianca(void *v) {
 }
 
 
+void* sai_adulto(void *v) {
+  int i, j;
+
+  for (i = 0; i < N_ITENS; i++) {
+    sem_wait(&pos_ocupada_adulto);
+
+	while (qtd_crianca > 3 * (qtd_adulto - 1));
+
+	for (j = 0; j < 3; j++){
+		sem_wait(&sem_crianca);
+	}
+
+	sem_post(&sem_adulto);
+
+    inicio_adulto = (inicio_adulto + 1) % N_ITENS;
+	qtd_adulto -= 1;
+    printf("Saindo adulto, item = %d. Tenho qtd = %d\n", buffer_adulto[inicio_adulto], qtd_adulto);
+    sem_post(&pos_vazia_adulto);
+    sleep(random() % 10);  /* Permite que a outra thread execute */  
+  }
+  return NULL;
+}
+
+
 int main() {
 
-	pthread_t thr_adulto, thr_crianca;
+	pthread_t thr_entra_adulto, thr_entra_crianca, thr_sai_adulto;
 
  	sem_init(&sem_crianca, 0, 0); // Inicia com 0 criancas
   	sem_init(&sem_adulto, 0, 1); // Inicia com 1 adulto
-  
-  	pthread_create(&thr_adulto, NULL, adulto, NULL);
-  	pthread_create(&thr_crianca, NULL, crianca, NULL);
 
-  	pthread_join(thr_adulto, NULL); 
-  	pthread_join(thr_crianca, NULL);
+	sem_init(&pos_vazia_adulto, 0, N_ITENS);
+	sem_init(&pos_ocupada_adulto, 0, 0);
+
+	sem_init(&pos_vazia_crianca, 0, N_ITENS);
+	sem_init(&pos_ocupada_crianca, 0, 0);
+  
+  	pthread_create(&thr_entra_adulto, NULL, entra_adulto, NULL);
+  	pthread_create(&thr_entra_crianca, NULL, entra_crianca, NULL);
+
+  	pthread_create(&thr_sai_adulto, NULL, sai_adulto, NULL);
+
+  	pthread_join(thr_entra_adulto, NULL); 
+  	pthread_join(thr_entra_crianca, NULL);
+  	pthread_join(thr_sai_adulto, NULL); 
 
   	sem_destroy(&sem_crianca);
   	sem_destroy(&sem_adulto);
+  	sem_destroy(&pos_ocupada_adulto);
+  	sem_destroy(&pos_vazia_adulto);
+  	sem_destroy(&pos_ocupada_crianca);
+  	sem_destroy(&pos_vazia_crianca);
   
   	return 0;
 }
